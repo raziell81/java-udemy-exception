5042eabdd38c4276141c6825261546844f132184
André Guaraci <andrecomplex@gmail.com>
2020-01-31 14:24:39 -0300

    Início do projeto

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = null;
+		
+		try {
+			
+		}
+		catch(Exception e){
+			
+		}		
+		
+		finally{
+			if(sc != null) {
+				sc.close();
+			}			
+		}		
+		
+	}
+
+}
